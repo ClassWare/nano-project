0ef24ce4342201e7939e88df197a8fc0d09c7b5d
martin <m.bazan@ntlworld.com>
2018-05-24 13:18:41 +0100

    Corrected windows startup for fs-server-csvtojson

--- 0-START.ps1
+++ 0-START.ps1
@@ -1,3 +1,3 @@
 cmd /c start powershell -NoExit -WindowStyle Minimized -Command { $Host.UI.RawUI.WindowTitle = "VU-MAIN" }
 Read-Host "Press ENTER"
-cmd /c start powershell -NoExit -WindowStyle Minimized -Command { $Host.UI.RawUI.WindowTitle = "FS-SERVER"; node fs-server-stream }
+cmd /c start powershell -NoExit -WindowStyle Minimized -Command { $Host.UI.RawUI.WindowTitle = "FS-SERVER"; node fs-server-csvtojson }
